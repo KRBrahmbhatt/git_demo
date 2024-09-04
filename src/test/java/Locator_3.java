222b40e2aa395536b26ece498d19cf0142fda8a2
KRBrahmbhatt <kaushalbrahmbhattt@gmail.com>
2024-09-04 11:18:15 +0530

    this is from india timezone commit ok

--- src/test/java/Locator_3.java
+++ src/test/java/Locator_3.java
@@ -7,7 +7,7 @@ public class Locator_3 {
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
         System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
-
+        System.out.println("hello brother");
 
     }
 }
